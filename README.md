3c1ea70a7b42f25a8eed025876f0b0e3c4cf8d4a
Roberto Lucas <121591437+VindexGundyr@users.noreply.github.com>
2024-04-01 18:28:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprender a fazer de fora a fora um projeto do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Olá meu caro irmão! Aqui está uma mensagem importante: eu so queria dizer que</h1>;
+  return (
+    <h1>
+      Olá meu caro irmão! Aqui está uma mensagem importante: eu so queria dizer
+      que
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log();
 }
 
 export default Home;
