51dd85036029e50a95ae44acd6a9714f73fb0f90
Roberto Lucas <121591437+VindexGundyr@users.noreply.github.com>
2024-04-15 21:36:45 +0000

    add `compose`config file with database service

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 function Home() {
   return (
     <h1>
+      {" "}
       Olá meu caro irmão! Aqui está uma mensagem importante: eu so queria dizer
       que
     </h1>
   );
 }
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log();
-}
-
 export default Home;
