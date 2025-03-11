5da619d855409faf6a06467bbc15c0a9c0f5d16a
Roberto Lucas <121591437+VindexGundyr@users.noreply.github.com>
2025-03-11 20:36:56 +0000

    style: format code with Prettier

--- models/migrator.js
+++ models/migrator.js
@@ -3,49 +3,49 @@ import { resolve } from "node:path";
 import database from "infra/database.js";
 
 const defaultMigrationOptions = {
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
 
-  async function listPendingMigrations() {
-    let dbClient;
-  
-    try {
-      dbClient = await database.getNewClient();
-  
-      const pendingMigrations = await migrationsRunner({
-        ...defaultMigrationOptions,
-        dbClient,
-      });
-      return pendingMigrations;
-    } finally {
-      await dbClient?.end();
-    }
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
   }
+}
 
-  async function runPendingMigrations() {
-    let dbClient;
-  
-    try {
-      dbClient = await database.getNewClient();
-  
-      const migratedMigrations = await migrationsRunner({
-        ...defaultMigrationOptions,
-        dbClient,
-        dryRun: false,
-      }); 
-      return migratedMigrations; 
-    } finally {
-      await dbClient?.end();
-    }
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
   }
+}
 
-  const migrator = {
-    listPendingMigrations,
-    runPendingMigrations,
-  }
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
 
-  export default migrator
+export default migrator;
