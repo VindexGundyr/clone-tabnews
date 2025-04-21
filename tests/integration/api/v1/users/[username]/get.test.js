173ed733d692162c8889ec52dc0226673c8726a4
Roberto Lucas <121591437+VindexGundyr@users.noreply.github.com>
2025-04-21 21:08:34 +0000

    fix: fix bug in `[username]/get.test.js`

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -45,7 +45,7 @@ describe("GET /api/v1/users/[username]", () => {
     });
     test("With case mismatch", async () => {
       const response1 = await fetch("http://localhost:3000/api/v1/users", {
-        method: "GET",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
@@ -79,7 +79,7 @@ describe("GET /api/v1/users/[username]", () => {
       expect(Date.parse(response2Body.created_at)).not.toBeNaN();
       expect(Date.parse(response2Body.updated_at)).not.toBeNaN();
     });
-    
+
     test("With nonexistent username", async () => {
       const response = await fetch(
         "http://localhost:3000/api/v1/UsuarioInexistente",
